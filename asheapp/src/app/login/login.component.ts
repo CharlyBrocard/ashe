ded334c12b04e6c6fd7136f76c1ed80de8e41a64
charly <brocard.charly@gmail.com>
2024-02-08 17:00:08 +0100

    je vais vriller

--- asheapp/src/app/login/login.component.ts
+++ asheapp/src/app/login/login.component.ts
@@ -1,7 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Apollo, Query, gql } from 'apollo-angular';
-import { AUTH } from '../graphlql.auth';
+
+const AUTH = gql`
+  query ($login: String!
+         $password: String:
+         ) { auth (dto: {login: $login,password: $password})
+      {
+        accessToken
+        id
+        code
+        name_first
+        name_last
+        description
+        mail
+        creation
+        modification
+        language
+      }
+  }
+`;
 
 @Component({
   selector: 'app-login',
@@ -19,34 +37,28 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('tape à gauche');
-  
   }
 
-  testData() {
+  async testData() {
     console.log(this.name.value);
     console.log(this.password.value);
-    this.apollo.watchQuery<Query>({
-      query: gql`
-      query {
-          auth (
-            dto: {
-              login: "ropo"
-              password: "password"
-            }
-          ) {
-            accessToken
-            id
-            code
-            name_first
-            name_last
-            description
-            mail
-            creation
-            modification
-            language
-          }
-        }
-      `
-    });  
+    try {
+      const response = await this.apollo.query<Query>({
+        query: AUTH,
+        variables: {
+          login: this.name.value,
+          password: this.password.value,
+        },
+      });
+
+      // Create session
+      sessionStorage.setItem('code', response.data.auth.code);
+      sessionStorage.setItem('jwt', response.data.auth.accessToken);
+      sessionStorage.setItem('id', response.data.auth.id);
+
+    } catch (error) {
+      console.error(error);
+    }
+ 
   }
 }
